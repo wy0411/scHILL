b532e74a2a0088b1eb9cb02d9611d38d0cb5afaf
Yi Wang <77912986+wys0411@users.noreply.github.com>
2025-12-03 18:07:39 +0800

    Update score-generation-covid19.py

--- fine-tune/step2/score-generation-covid19.py
+++ fine-tune/step2/score-generation-covid19.py
@@ -317,7 +317,8 @@ def main(random_state=777):
                     if (torch.argmax(outputs, dim=1).item() == 0):  # disease
                         outputs = torch.nn.functional.softmax(model2(tensors1), dim=1).mean(dim=0).unsqueeze(0)
                         if labels2 == 0:
-                            print(f'file:{file_name}, point:{outputs[0, 1].item() + outputs[0, 0].item() * 2}')
+                            with open(f"{i}/{i}.log", "a") as f:
+                                f.write(f'file:{file_name}, point:{outputs[0, 1].item() + outputs[0, 0].item() * 2}\n')
 
     folders = ["1", "2", "3", "4", "5"]
     records = []
