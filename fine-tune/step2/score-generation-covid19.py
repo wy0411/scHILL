60c28725cecd78f9a2e2dd65d801bd8d986ab701
Yi Wang <77912986+wys0411@users.noreply.github.com>
2025-12-03 17:20:39 +0800

    Update score-generation-covid19.py

--- fine-tune/step2/score-generation-covid19.py
+++ fine-tune/step2/score-generation-covid19.py
@@ -314,7 +314,6 @@ def main(random_state=777):
                         device), file_name
                     tensors1 = tensors.squeeze(0)
                     outputs = torch.nn.functional.softmax(model(tensors1), dim=1).mean(dim=0).unsqueeze(0)
-                    print(f'file:{file_name},disease possibility:{outputs[0, 0].item()}')
                     if (torch.argmax(outputs, dim=1).item() == 0):  # disease
                         outputs = torch.nn.functional.softmax(model2(tensors1), dim=1).mean(dim=0).unsqueeze(0)
                         print(f'file:{file_name}, point:{outputs[0, 1].item() + outputs[0, 0].item() * 2}')
